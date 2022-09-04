af5d3c2b9a55f67c79087d6f5847ded8b2712fb5
matador19 <alexmaina1997@gmail.com>
2022-09-04 19:26:37 +0300

    login page works

--- websystem/web/urls.py
+++ websystem/web/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home,name="dashboard"),
+    path("login",views.loginform,name="login"),
+    path("admindashboard",views.admindash,name="admindash"),
+    path("supervisordashboard",views.supdash,name="supdash")
 ]
