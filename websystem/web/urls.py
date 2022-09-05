9bcc266496166a3f8d2da1b0e5189c204bbdbcd4
matador19 <alexmaina1997@gmail.com>
2022-09-05 21:22:57 +0300

    logout and login complete

--- websystem/web/urls.py
+++ websystem/web/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home,name="dashboard"),
+    path("", views.home,name="home"),
     path("login",views.loginform,name="login"),
+    path("logout",views.logoutsuccess,name="logout"),
     path("admindashboard",views.admindash,name="admindash"),
     path("supervisordashboard",views.supdash,name="supdash")
 ]
